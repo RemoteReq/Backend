ba88623601573063d2e8a7db402ebaa3d7462746
ryanferrer <ryferr8122@gmail.com>
2020-05-12 23:22:27 -0500

    configured routes

--- server/server.js
+++ server/server.js
@@ -7,14 +7,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3030;
 
-// User history to intecept client requests and forward to React Router history
+// import route handlers
+const signup = require('./routes/api/Signup.js');
+const user = require('./routes/api/User.js');
+const signin = require('./routes/api/Signin.js');
+
+// Use history to intecept client requests and forward to React Router history
 app.use(history());
 
 // express middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static('dist'));
 
+
+// routes
+app.use('/api/signin', signin);
+app.use('/api/signup', signup);
+app.use('/api/user', user);
 
 app.get('/', (req, res) => {
   res.status(200).send();
