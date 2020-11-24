4392fe942f2f4c3dd645b450d450b4f16ec52e90
subhankarCN <subhankar.ray@capitalnumbers.com>
2020-11-24 20:47:55 +0530

    SJ timer  cahnge

--- server/api/schedulejob/scheduleJob.js
+++ server/api/schedulejob/scheduleJob.js
@@ -10,6 +10,7 @@ const {
     isHired,
     autoUpdateHiringStatus
 } = require('../../../database/controllers/scheduleJob')
+
 const {
     mailToEmployerForCandidateMatchTest, 
     mailForTwoDaysLeftTest,
@@ -19,36 +20,36 @@ const {
     autoUpdateHiringStatusTest
 } = require('../../../database/controllers/scheduleJobForTesting')
 
-// router.post('/mailToEmployerForCandidateMatch', (req, res)=>{
-//     mailToEmployerForCandidateMatch(req,res)
-// })
-
-// router.post('/mailForTwoDaysLeft', mailForTwoDaysLeft)
-
-// router.post('/checkHiredOrNot', checkHiredOrNot)
-
-// router.post('/checkExpiredJob', checkExpiredJob)
-
-// router.post('/isHired', isHired)
-
-// router.post('/employerNotRespForHiring', autoUpdateHiringStatus)
-
-
-
-//For Testing Purpose
-router.post('/checkExpiredJob', checkExpiredJobTest)
-
-router.post('/checkHiredOrNot', checkHiredOrNotTest)
-
-router.post('/isHired', isHiredTest)
-
-router.post('/employerNotRespForHiring', autoUpdateHiringStatusTest)
-
-router.post('/mailForTwoDaysLeft', mailForTwoDaysLeftTest)
-
 router.post('/mailToEmployerForCandidateMatch', (req, res)=>{
-    mailToEmployerForCandidateMatchTest(req,res)
+    mailToEmployerForCandidateMatch(req,res)
 })
+
+router.post('/mailForTwoDaysLeft', mailForTwoDaysLeft)
+
+router.post('/checkHiredOrNot', checkHiredOrNot)
+
+router.post('/checkExpiredJob', checkExpiredJob)
+
+router.post('/isHired', isHired)
+
+router.post('/employerNotRespForHiring', autoUpdateHiringStatus)
+
+
+
+//** For Testing Purpose **
+// router.post('/checkExpiredJob', checkExpiredJobTest)
+
+// router.post('/checkHiredOrNot', checkHiredOrNotTest)
+
+// router.post('/isHired', isHiredTest)
+
+// router.post('/employerNotRespForHiring', autoUpdateHiringStatusTest)
+
+// router.post('/mailForTwoDaysLeft', mailForTwoDaysLeftTest)
+
+// router.post('/mailToEmployerForCandidateMatch', (req, res)=>{
+//     mailToEmployerForCandidateMatchTest(req,res)
+// })
 
 module.exports = router
 
--- server/cron.js
+++ server/cron.js
@@ -4,95 +4,8 @@ var unirest = require('unirest');
 let API_URL = (process.env.HOST_TYPE == 'local')? 'http://localhost:3030/' : (process.env.HOST_TYPE == 'dev')? 'http://3.21.186.204:3030/' : 'http://18.191.219.131:3030/';
 
 //find out all of the jobs which was expired(21 days period over) - everday at 12:01 AM
-// cron.schedule('1 0 * * *', () => {
-//     console.log('find out all of the jobs which was expired(21 days period over) - everday at 12:01 AM');
-//     unirest
-//       .post(API_URL+'api/scheduleJob/checkExpiredJob')
-//       .headers({'Content-Type': 'application/json'})
-//       .send({ 
-          
-//       })
-//       .then(async(response) => {
-//           console.log(response.body)
-//       })
-//       .catch(err => {
-//           console.log(err)
-//       })
-// });
-
-// After expired job sends mail for hiring complete or not (7 days continue until employer's answerd) - At 1:00 AM
-// cron.schedule('0 1 * * *', () => {
-//     console.log('After expired job sends mail for hiring complete or not (7 days continue until employer\'s answerd) - At 1:00 AM');
-//     unirest
-//       .post(API_URL+'api/scheduleJob/checkHiredOrNot')
-//       .headers({'Content-Type': 'application/json'})
-//       .send({ 
-          
-//       })
-//       .then(async(response) => {
-//           console.log(response.body)
-//       })
-//       .catch(err => {
-//           console.log(err)
-//       })
-// });
-
-// Auto charged for hiring if employer not responsed after 7 days - At 12:40 AM
-// cron.schedule('40 0 * * *', () => {
-//     console.log('Auto charged for hiring if employer not responsed after 7 days - At 12:40 AM');
-//     unirest
-//       .post(API_URL+'api/scheduleJob/employerNotRespForHiring')
-//       .headers({'Content-Type': 'application/json'})
-//       .send({ 
-          
-//       })
-//       .then(async(response) => {
-//           console.log(response.body)
-//       })
-//       .catch(err => {
-//           console.log(err)
-//       })
-// });
-
-// sends mail for 48 hours reminder - at 12:20 AM
-// cron.schedule('20 0 * * *', () => {
-//     console.log('sends mail for 48 hours reminder - at 12:20 AM');
-//     unirest
-//       .post(API_URL+'api/scheduleJob/mailForTwoDaysLeft')
-//       .headers({'Content-Type': 'application/json'})
-//       .send({ 
-          
-//       })
-//       .then(async(response) => {
-//           console.log(response.body)
-//       })
-//       .catch(err => {
-//           console.log(err)
-//       })
-// });
-
-// Cron run every 8 hours for check matching candidate
-// cron.schedule('0 */8 * * *', () => {
-//     console.log('Cron run every 8 hours for check matching candidate');
-//     unirest
-//       .post(API_URL+'api/scheduleJob/mailToEmployerForCandidateMatch')
-//       .headers({'Content-Type': 'application/json'})
-//       .send({ 
-          
-//       })
-//       .then(async(response) => {
-//           console.log(response.body)
-//       })
-//       .catch(err => {
-//           console.log(err)
-//       })
-// });
-
-
-
-// for Testing Purpose
-
-cron.schedule('*/30 * * * *', () => { // job expired check every 30 minutes 
+cron.schedule('1 0 * * *', () => {
+    console.log('find out all of the jobs which was expired(21 days period over) - everday at 12:01 AM');
     unirest
       .post(API_URL+'api/scheduleJob/checkExpiredJob')
       .headers({'Content-Type': 'application/json'})
@@ -107,7 +20,9 @@ cron.schedule('*/30 * * * *', () => { // job expired check every 30 minutes
       })
 });
 
-cron.schedule('*/10 * * * *', () => { // check hiring or not every 10 minutes
+// After expired job sends mail for hiring complete or not (7 days continue until employer's answerd) - At 1:00 AM
+cron.schedule('0 1 * * *', () => {
+    console.log('After expired job sends mail for hiring complete or not (7 days continue until employer\'s answerd) - At 1:00 AM');
     unirest
       .post(API_URL+'api/scheduleJob/checkHiredOrNot')
       .headers({'Content-Type': 'application/json'})
@@ -122,22 +37,26 @@ cron.schedule('*/10 * * * *', () => { // check hiring or not every 10 minutes
       })
 });
 
-cron.schedule('*/5 * * * *', () => { // check employer not responde in hiring status for every 5 minutes
+// Auto charged for hiring if employer not responsed after 7 days - At 12:40 AM
+cron.schedule('40 0 * * *', () => {
+    console.log('Auto charged for hiring if employer not responsed after 7 days - At 12:40 AM');
     unirest
-        .post(API_URL+'api/scheduleJob/employerNotRespForHiring')
-        .headers({'Content-Type': 'application/json'})
-        .send({ 
-            
-        })
-        .then(async(response) => {
-            console.log(response.body)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+      .post(API_URL+'api/scheduleJob/employerNotRespForHiring')
+      .headers({'Content-Type': 'application/json'})
+      .send({ 
+          
+      })
+      .then(async(response) => {
+          console.log(response.body)
+      })
+      .catch(err => {
+          console.log(err)
+      })
 });
 
-cron.schedule('*/3 * * * *', () => { // reminder between 10-5 mints left (check every 3 mints) 
+// sends mail for 48 hours reminder - at 12:20 AM
+cron.schedule('20 0 * * *', () => {
+    console.log('sends mail for 48 hours reminder - at 12:20 AM');
     unirest
       .post(API_URL+'api/scheduleJob/mailForTwoDaysLeft')
       .headers({'Content-Type': 'application/json'})
@@ -152,9 +71,9 @@ cron.schedule('*/3 * * * *', () => { // reminder between 10-5 mints left (check 
       })
 });
 
-
-cron.schedule('*/8 * * * *', () => { //check matched candidates list every 8 minutes
-    
+// Cron run every 8 hours for check matching candidate
+cron.schedule('0 */8 * * *', () => {
+    console.log('Cron run every 8 hours for check matching candidate');
     unirest
       .post(API_URL+'api/scheduleJob/mailToEmployerForCandidateMatch')
       .headers({'Content-Type': 'application/json'})
@@ -168,3 +87,84 @@ cron.schedule('*/8 * * * *', () => { //check matched candidates list every 8 min
           console.log(err)
       })
 });
+
+
+
+// for Testing Purpose
+
+// cron.schedule('*/30 * * * *', () => { // job expired check every 30 minutes 
+//     unirest
+//       .post(API_URL+'api/scheduleJob/checkExpiredJob')
+//       .headers({'Content-Type': 'application/json'})
+//       .send({ 
+          
+//       })
+//       .then(async(response) => {
+//           console.log(response.body)
+//       })
+//       .catch(err => {
+//           console.log(err)
+//       })
+// });
+
+// cron.schedule('*/10 * * * *', () => { // check hiring or not every 10 minutes
+//     unirest
+//       .post(API_URL+'api/scheduleJob/checkHiredOrNot')
+//       .headers({'Content-Type': 'application/json'})
+//       .send({ 
+          
+//       })
+//       .then(async(response) => {
+//           console.log(response.body)
+//       })
+//       .catch(err => {
+//           console.log(err)
+//       })
+// });
+
+// cron.schedule('*/5 * * * *', () => { // check employer not responde in hiring status for every 5 minutes
+//     unirest
+//         .post(API_URL+'api/scheduleJob/employerNotRespForHiring')
+//         .headers({'Content-Type': 'application/json'})
+//         .send({ 
+            
+//         })
+//         .then(async(response) => {
+//             console.log(response.body)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+// });
+
+// cron.schedule('*/3 * * * *', () => { // reminder between 10-5 mints left (check every 3 mints) 
+//     unirest
+//       .post(API_URL+'api/scheduleJob/mailForTwoDaysLeft')
+//       .headers({'Content-Type': 'application/json'})
+//       .send({ 
+          
+//       })
+//       .then(async(response) => {
+//           console.log(response.body)
+//       })
+//       .catch(err => {
+//           console.log(err)
+//       })
+// });
+
+
+// cron.schedule('*/8 * * * *', () => { //check matched candidates list every 8 minutes
+    
+//     unirest
+//       .post(API_URL+'api/scheduleJob/mailToEmployerForCandidateMatch')
+//       .headers({'Content-Type': 'application/json'})
+//       .send({ 
+          
+//       })
+//       .then(async(response) => {
+//           console.log(response.body)
+//       })
+//       .catch(err => {
+//           console.log(err)
+//       })
+// });
