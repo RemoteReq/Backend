0aa43510751dcfe770384d2f21270f1d73d1068a
ryanferrer <ryferr8122@gmail.com>
2021-02-04 15:47:49 -0600

    move cors policy

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var cors = require('cors');
 const history = require('connect-history-api-fallback');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-require('../gateway/passportSetup')
+require('../gateway/passportSetup');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -18,13 +18,10 @@ const global = require('./api/auth/global');
 const socialLogin = require('./api/auth/socialLogin');
 const user = require('./api/auth/user');
 const signin = require('./api/auth/signIn.js');
-const jobs = require('./api/dashboard/jobs.js')
-const employers = require('./api/Employer/employer')
-const scheduleJob = require('./api/schedulejob/scheduleJob')
-const demoRequestJob = require('./api/auth/demoRequest')
-
-//CORS
-app.use(cors())
+const jobs = require('./api/dashboard/jobs.js');
+const employers = require('./api/Employer/employer');
+const scheduleJob = require('./api/schedulejob/scheduleJob');
+const demoRequestJob = require('./api/auth/demoRequest');
 
 // Use history to intecept client requests and forward to React Router history
 // app.use(history());
@@ -32,7 +29,7 @@ app.use(cors())
 // express middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const {tokenValidityChecking, tokenValidityCheckingForEmp} = require('./authentication')
+const {tokenValidityChecking, tokenValidityCheckingForEmp} = require('./authentication');
 app.use(require('express-session')({
   secret: process.env.GOOGLE_SESSION_SECRET,
   resave: true,
@@ -40,6 +37,9 @@ app.use(require('express-session')({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+//CORS
+app.use(cors());
 
 // require('./cron')
 // routes
